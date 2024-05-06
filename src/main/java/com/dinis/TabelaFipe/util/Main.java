9924919599e387a2e348bf387b10854673fc89dd
dinis <dinisde@gmail.com>
2024-05-06 14:13:10 -0300

    finalizei a impressao das buscas

--- src/main/java/com/dinis/TabelaFipe/util/Main.java
+++ src/main/java/com/dinis/TabelaFipe/util/Main.java
@@ -2,16 +2,19 @@ package com.dinis.TabelaFipe.util;
 
 import com.dinis.TabelaFipe.domin.DataFipe;
 import com.dinis.TabelaFipe.domin.DataModels;
+import com.dinis.TabelaFipe.domin.Vehicle;
 import com.dinis.TabelaFipe.service.ApiService;
 import com.dinis.TabelaFipe.service.DataConverter;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     private String optionTypes;
     private final String BASE_PATH = "https://parallelum.com.br/fipe/api/v1/";
     protected Scanner input = new Scanner(System.in);
+    private final DataConverter converter = new DataConverter();
 
         public void start(){
 
@@ -32,25 +35,59 @@ public class Main {
                        .orElse(null);
 
                if(optionTypes != null){
-                   System.out.println(optionTypes);
                    String path =  BASE_PATH + optionTypes + "/marcas";
                    ApiService api = new ApiService();
                    var json = api.fetchDataFromApi(path);
-                   DataConverter converter = new DataConverter();
                    var models = converter.getList(json, DataFipe.class);
-                   models.stream()
-                           .sorted(Comparator.comparing(DataFipe::cod))
-                           .forEach(System.out::println);
 
-                   System.out.println(Messages.BRANDS_MENU);
-                   String codBrands  = input.nextLine();
+                   List<String> codList = models.stream()
+                           .sorted(Comparator.comparing(DataFipe::name))
+                           .peek(model -> System.out.println(model.cod() + ": " + model.name()))
+                           .map(DataFipe::cod)
+                           .toList();
+                   System.out.println("1");
+
+                   String codBrands;
+                   do {
+                       System.out.println(Messages.BRANDS_MENU);
+                       codBrands = input.nextLine();
+
+                       if (!codList.contains(codBrands)) {
+                           System.out.println(Messages.COD_ERROR);
+                       }
+                   } while (!codList.contains(codBrands));
 
                    path = path + "/" + codBrands + "/modelos";
                    json = api.fetchDataFromApi(path);
                    var modelList = converter.fetchData(json, DataModels.class);
                    modelList.modesls().stream()
-                           .sorted(Comparator.comparing(DataFipe::cod))
-                           .forEach(System.out::println);
+                                   .sorted(Comparator.comparing(DataFipe::name))
+                                   .forEach(System.out::println);
+
+                   System.out.println(Messages.FILTER_VEHICLE);
+                   String filterVehicle = input.nextLine();
+
+                    List<DataFipe> filteModels = modelList.modesls().stream()
+                            .filter(m -> m.name().toLowerCase().contains(filterVehicle.toLowerCase()))
+                            .toList();
+                    filteModels.forEach(System.out::println);
+
+                   System.out.println(Messages.MODELS_MENU);
+                   String modelCod = input.nextLine();
+                   path = path + "/" + modelCod + "/anos";
+                   json = api.fetchDataFromApi(path);
+                   List<DataFipe> years = converter.getList(json, DataFipe.class);
+                   List<Vehicle> vehicles = new ArrayList<>();
+
+                   for (int i = 0; i < years.size(); i++) {
+                       String pathYears = path + "/" + years.get(i).cod();
+                       json = api.fetchDataFromApi(pathYears);
+                       Vehicle vehicle = converter.fetchData(json, Vehicle.class);
+                       vehicles.add(vehicle);
+                   }
+
+                   System.out.println(Messages.VEHICLE_INFORMATION);
+                   vehicles.forEach(System.out::println);
 
                    break;
                }else{
--- src/main/java/com/dinis/TabelaFipe/util/Messages.java
+++ src/main/java/com/dinis/TabelaFipe/util/Messages.java
@@ -3,10 +3,11 @@ package com.dinis.TabelaFipe.util;
 public class Messages {
 
     protected static final String TYP_MENU = "Escolha o tipo de veículo.";
-    protected static final String BRANDS_MENU = "Informe o codigo da marca.";
-    protected static final String MODELS_MENU = "Escolha o modelo.";
-    protected static final String MENU = "\ncarro\nmoto\ncaminhao\n";
-    protected static final String ERROR_TYPES = "Não Encontrado";
-
-
+    protected static final String BRANDS_MENU = "\nInforme o codigo da marca.";
+    protected static final String MODELS_MENU = "\nDigite codigo do modelo.";
+    protected static final String MENU = "\ncarros\nmotos\ncaminhoes\n";
+    protected static final String ERROR_TYPES = "\nNão Encontrado";
+    protected static final String COD_ERROR = "\nCódigo não existe. Tente novamente:";
+    protected static final String FILTER_VEHICLE = "\nDigite o nome do veiculo para busca";
+    protected static final String VEHICLE_INFORMATION = "\nInformações do Veiculo";
 }
